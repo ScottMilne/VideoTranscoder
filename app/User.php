0cd6e5f8d99b8e027a88f2e4e4861ec444adefbb
scottmilne <scott.milne6@gmail.com>
2022-03-14 15:52:08 +0000

    Added stripe routes

--- app/User.php
+++ app/User.php
@@ -3,10 +3,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('stripe', 'StripePaymentController@stripe');
+Route::post('stripe', 'StripePaymentController@stripePost')->name('stripe.post');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
